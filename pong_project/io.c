f73147b63f2a215a76f55b2b4a3e0e4699ee6d07
Casper Johansson <147274068+SnakiestCarrot@users.noreply.github.com>
2024-02-09 20:12:14 +0100

    Added files from lab 3 into the project
    Created the basic starting point

---
+++ pong_project/io.c
@@ -0,0 +1,32 @@
+
+#include <stdint.h>
+#include <pic32mx.h>
+#include "mipslab.h"
+
+// returns switch 1-4 in bits 0-3
+int getsw ( void ) {
+    return (PORTD & 0x0f00) >> 8;
+}
+
+// returns buttons 2-4 in bit 0-2
+int getbtns ( void ) {
+    return (PORTD & 0x0e0) >> 5;
+}
+
+// returns 1 if pressed, else 0
+int btn4pressed ( void ) {
+    return (getbtns() & 0x4) >> 2;
+}
+
+int btn3pressed ( void ) {
+    return (getbtns() & 0x2) >> 1;
+}
+
+int btn2pressed ( void ) {
+    return (getbtns() & 0x1);
+}
+
+// special case for btn1 since it is a different bit than 2-4
+int btn1pressed ( void ) {
+    return (PORTF & 0x2) >> 1;
+}
